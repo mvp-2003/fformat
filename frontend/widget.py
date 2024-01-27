bbbfb92604a6dacb89dcb36e7ff0323000512c1b
Rishabh <rishabh.babi@gmail.com>
2024-01-28 00:36:40 +0530

    lol

--- frontend/widget.py
+++ frontend/widget.py
@@ -3,7 +3,7 @@ import customtkinter as ctk
 import os
 import sys
 sys.path.insert(1, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'backend'))
-from light import *
+from light import addfile
 
 app = ctk.CTk()
 app.title("FFormating")
