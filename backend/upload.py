1c292e08dd644142be1318d8b2f6486ba150d5b8
Rishabh <rishabh.babi@gmail.com>
2024-01-29 00:57:00 +0530

    Using some brains

--- backend/upload.py
+++ backend/upload.py
@@ -1,9 +1,4 @@
-import os
-import sys
 from tkinter import filedialog
-
-sys.path.insert(1, os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'frontend'))
-
 def selectfile():
     # This function is called when the user clicks the "Choose file" button
     pass
