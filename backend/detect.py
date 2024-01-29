06005e6c73f1af0d0b21b6694fd2bc9c6c216ba0
Rishabh <rishabh.babi@gmail.com>
2024-01-30 00:47:18 +0530

    Will be implemented in the frontend directory.

--- backend/detect.py
+++
@@ -1,6 +0,0 @@
-from frontend.widget import extension_field, entry_field
-from backend.light import addfile
-
-def fill_box():
-    message = addfile(entry_field)
-    extension_field.insert(0, message)
