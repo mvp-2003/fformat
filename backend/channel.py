c2f733b570b686ee0438da4919a0506dc60b25af
Rishabh <rishabh.babi@gmail.com>
2024-01-30 18:53:10 +0530

    Getting the name of the image to later return

--- backend/channel.py
+++ backend/channel.py
@@ -2,10 +2,12 @@
 from backend.conversions.jpg2png import jpg_to_png
 from backend.conversions.png2jpg import png_to_jpg
 from backend.conversions.jpg2jpeg import jpg_to_jpeg'''
-
+import os
 
 def convert(entry_field, type_selection):
+    filepath = entry_field.get()
     typec = type_selection.get()
+    name = os.path.splitext(filepath)[0]
 
     if typec == ".jpg":
         # jpeg_to_jpg()
