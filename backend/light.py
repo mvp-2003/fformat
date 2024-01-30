8fea323ae782d0bddd52b926210d582a50f2f3a8
Rishabh <rishabh.babi@gmail.com>
2024-01-30 12:35:10 +0530

    Adding no file selection

--- backend/light.py
+++ backend/light.py
@@ -1,7 +1,10 @@
 import os
+from tkinter import font
 
-def addfile(entry_field, extension_label):
+def addfile(entry_field, extension_label, no_file_label):
     filepath = entry_field.get()
+    if(filepath == ""):
+        no_file_label.configure(text = "No file selected", text_color = "red", font=("Ariel", 16))
     entry_field.delete(0, 'end')
     extension = os.path.splitext(filepath)[1]
     extension_label.configure(text = extension)
