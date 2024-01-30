7dc7ff4ddd82b83a4c317347714f233128e9abf2
Rishabh <rishabh.babi@gmail.com>
2024-01-30 23:45:37 +0530

    Fixed UX

--- backend/light.py
+++ backend/light.py
@@ -5,6 +5,7 @@ def addfile(entry_field, extension_label, no_file_label):
     filepath = entry_field.get()
     if(filepath == ""):
         no_file_label.configure(text = "No file selected", text_color = "red", font=("Ariel", 16))
-    entry_field.delete(0, 'end')
-    extension = os.path.splitext(filepath)[1]
-    extension_label.configure(text = extension)
+    else:
+        entry_field.delete(0, 'end')
+        extension = os.path.splitext(filepath)[1]
+        extension_label.configure(text = extension)
