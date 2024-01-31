41f2fde1043053a4d7802dd128e52a53b272414a
Rishabh <rishabh.babi@gmail.com>
2024-02-01 00:37:48 +0530

    just commit

--- backend/conversions/png2jpg.py
+++ backend/conversions/png2jpg.py
@@ -4,4 +4,5 @@ def png_to_jpg(entry_field, filename, new_file_label):
     image = Image.open(entry_field.get())
     image = image.convert('RGB')
     image.save(filename + ".jpg")
-    new_file_label.configure(text = filename + ".jpg")
+    text = filename + ".jpg"
+    new_file_label.configure(text = text)
