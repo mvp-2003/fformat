08e1db51998f6b9856ceba17a5807b330286356c
Rishabh <rishabh.babi@gmail.com>
2024-01-31 19:26:41 +0530

    Updated the code to keep the message if no file is selected

--- frontend/filedialog.py
+++ frontend/filedialog.py
@@ -2,5 +2,6 @@ from customtkinter import filedialog
 
 def selectfile(entry_field):
     filename = filedialog.askopenfilename()
-    entry_field.delete(0, 'end')
-    entry_field.insert(0, filename)
+    if filename:
+        entry_field.delete(0, 'end')
+        entry_field.insert(0, filename)
