4d1be412dd88b1421eb5becb865126d8fe81b410
Rishabh <rishabh.babi@gmail.com>
2024-02-01 00:50:20 +0530

    Fixing mistakes

--- backend/conversions/png2jpg.py
+++ backend/conversions/png2jpg.py
@@ -1,8 +1,5 @@
 from PIL import Image
 
 def png_to_jpg(entry_field, filename, new_file_label):
-    image = Image.open(entry_field.get())
-    image = image.convert('RGB')
-    image.save(filename + ".jpg")
-    text = filename + ".jpg"
-    new_file_label.configure(text = text)
+    # Fixing mistakes
+    pass
