8b4a1f3e2637fab8c6a86565f8826ccf75378ee9
Rishabh <rishabh.babi@gmail.com>
2024-02-01 22:12:21 +0530

    Checking for file similarity

--- backend/channel.py
+++ backend/channel.py
@@ -5,10 +5,9 @@ from backend.conversions.png2jpg import png_to_jpg
 from backend.conversions.jpg2jpeg import jpg_to_jpeg
 import os
 
-
 def convert(new_file_label, type_selection, file_name_label, extension_label):
     if file_name_label.cget("text") == "" or extension_label.cget("text") == "":
         new_file_label.configure(text="No file selected / uploaded", text_color="red")
     else:
-        newfile = ""
-        new_file_label.configure(text = newfile)
+        if extension_label.cget("text") == type_selection.get():
+            new_file_label.configure(text="File already in this format", text_color="red")
