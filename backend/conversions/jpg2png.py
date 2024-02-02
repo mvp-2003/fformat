f147b385bc7e717dd50ff5832ca6281895f649f7
Rishabh <rishabh.babi@gmail.com>
2024-02-02 19:44:49 +0530

    will be recreating more

--- backend/conversions/jpg2png.py
+++ backend/conversions/jpg2png.py
@@ -2,6 +2,7 @@ from PIL import Image
 
 def jpg_to_png(entry_field):
     image = Image.open(entry_field)
+    image.show()
     if image.mode != "RGB":
         image = image.convert("RGB")
-    nfile = image.save(entry_field+".png", "PNG")
+    image.save(entry_field+".png", "PNG")
