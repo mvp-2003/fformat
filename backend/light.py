c296b51d6a51ce52d0e80fd1bc85b0dbf8a7653e
Rishabh <rishabh.babi@gmail.com>
2024-02-02 13:57:51 +0530

    Fixed the delete message on entry field after upload too

--- backend/light.py
+++ backend/light.py
@@ -9,4 +9,5 @@ def addfile(entry_field, file_name_label, extension_label, no_file_label):
         file_name, extension = os.path.splitext(file_name_with_extension)
         file_name_label.configure(text = file_name)
         extension_label.configure(text = extension)
+        entry_field.delete(0, "end")
         no_file_label.configure(text = "")
