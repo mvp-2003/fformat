dc6470fd0c1b2acec8554c163b41d3fad0deb264
Rishabh <rishabh.babi@gmail.com>
2024-02-02 19:57:22 +0530

    Too many backend module has to be imported, so fixed the code

--- backend/channel.py
+++ backend/channel.py
@@ -1,7 +1,4 @@
-from backend.conversions.jpeg2jpg import jpeg_to_jpg
-from backend.conversions.jpg2png import jpg_to_png
-from backend.conversions.png2jpg import png_to_jpg
-from backend.conversions.jpg2jpeg import jpg_to_jpeg
+from backend.conversions import *
 import os
 
 def convert():
