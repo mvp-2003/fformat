1eb4e4e05f2d6a20ee9a24d56832dc82aa7a9907
Rishabh <rishabh.babi@gmail.com>
2024-02-02 15:31:53 +0530

    fixig save button

--- frontend/savefile.py
+++ frontend/savefile.py
@@ -6,6 +6,5 @@ def choose_folder(selected_folder_name):
     folder = filedialog.askdirectory()
     selected_folder_name.configure(text=folder)
 
-def savefile(new_file_label, saved_file_label):
-    # Get the new file's path
-    pass
+def savefile(new_file_label, saved_file_label, type_selection):
+    filex = filedialog.asksaveasfilename(defaultextension = type_selection.get())
